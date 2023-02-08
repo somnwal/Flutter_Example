5cccbea6370d706b49b46e1f872ce9c89bd93fcf
솜놜 <somnwal.kor@gmail.com>
2023-02-08 23:12:34 +0900

    20230208

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+import './question.dart';
+import './answer.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var index = 0;
+
+  void answerQuestion() {
+    setState(() {
+      index = index + 1;
+    });
+
+    print(index);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    var questions = ['ab', 'b', 'c'];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Test")),
+        body: Column(children: [
+          Question(
+            questions[index],
+          ),
+          Answer(answerQuestion),
+          Answer(answerQuestion),
+          Answer(answerQuestion),
+        ]),
+      ),
+    );
   }
 }
