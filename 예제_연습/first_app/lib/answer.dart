74be1e8c12ecabdf730504b15816655816f9ad20
솜놜 <somnwal.kor@gmail.com>
2023-02-13 18:51:24 +0900

    20230213

--- 예제_연습/first_app/lib/answer.dart
+++ 예제_연습/first_app/lib/answer.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  final VoidCallback selectHandler;
+  final Function selectHandler;
+  final String answerText;
 
-  const Answer(this.selectHandler, {super.key});
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: double.infinity,
-        child: ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.blue),
-          ),
-          onPressed: selectHandler,
-          child: Text('Answer 1'),
-        ));
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
   }
 }
--- 예제_연습/first_app/lib/question.dart
+++ 예제_연습/first_app/lib/question.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
-  Question(this.questionText, {super.key});
+  Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: double.infinity,
-        margin: const EdgeInsets.all(10),
-        child: Text(
-          questionText,
-          style: const TextStyle(
-            fontSize: 28,
-          ),
-          textAlign: TextAlign.center,
-        ));
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
