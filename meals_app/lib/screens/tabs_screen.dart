0eaa29defaca0a0b98caa192c27f4a17e26ce67e
솜놜 <somnwal.kor@gmail.com>
2023-02-20 20:51:31 +0900

    Drawer 생성

--- meals_app/lib/screens/tabs_screen.dart
+++ meals_app/lib/screens/tabs_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/favorites_screen.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabScreen extends StatefulWidget {
   const TabScreen({super.key});
@@ -12,34 +13,51 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Favorties',
+    },
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Meals'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: 'Favorites',
-              ),
-            ],
-          ),
+          title: Text(_pages[_selectedPageIndex]['title'] as String),
         ),
-        body: const TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoritesScreen(),
+        drawer: MainDrawer(),
+        body: _pages[_selectedPageIndex]['page'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          unselectedItemColor: Theme.of(context).primaryColorDark,
+          selectedItemColor: Colors.white,
+          backgroundColor: Theme.of(context).primaryColor,
+          currentIndex: _selectedPageIndex,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'category',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'favorites',
+            ),
           ],
         ),
       ),
---
+++ meals_app/lib/widgets/main_drawer.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget buildListTile(String title, IconData icon, Function tapHandler) {
+      return ListTile(
+        leading: Icon(
+          icon,
+          size: 26,
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onTap: () => tapHandler,
+      );
+    }
+
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Cooking Up',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).accentColor,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          buildListTile(
+            'Meals',
+            Icons.restaurant,
+            () => {},
+          ),
+          buildListTile(
+            'Filter',
+            Icons.settings,
+          ),
+        ],
+      ),
+    );
+  }
+}
